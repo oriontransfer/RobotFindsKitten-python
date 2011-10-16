b4a0a5af1799bfbd56ae302ff0301d35a906e678
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2011-10-17 01:53:58 +1300

    Tidy up code for release.

--- metal.py
+++ metal.py
@@ -1,185 +1,208 @@
+
+# metal is a simple python network library I wrote years ago.
+
+# Copyright (c) 2003, 2011 by Samuel Williams.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import asyncore, socket, time, sys, string, struct, random, time;
 from array import array;
 
-def hexdump (Data):
+def hexdump(Data):
     Counter = 0;
     Div = 4;
     Buffer = "";
     LineData = ""
     for Ch in Data:
-        if (Counter % 16) == 0:
-            Buffer += (hex (Counter) + " ").rjust (7) + " ";
+        if(Counter % 16) == 0:
+            Buffer +=(hex(Counter) + " ").rjust(7) + " ";
         Div -= 1;
-        Buffer += string.zfill ((hex ( ord(Ch)) [2:]), 2) + " ";
-        if (ord (Ch) >= 32) & (ord (Ch) < 127):
+        Buffer += string.zfill((hex( ord(Ch)) [2:]), 2) + " ";
+        if(ord(Ch) >= 32) &(ord(Ch) < 127):
             LineData += Ch;
         else:
             LineData += ".";
         if Div == 0:
             Div = 4;
             Buffer += " ";
-        if (Counter % 16) == 15:
+        if(Counter % 16) == 15:
             Buffer += " " + LineData + "\n";
             LineData = "";
         Counter += 1;
     Length = Counter % 16;
-    return Buffer + LineData.rjust (53 - ((Length * 3) + int(Length / 4)) + len (LineData));
+    return Buffer + LineData.rjust(53 -((Length * 3) + int(Length / 4)) + len(LineData));
 
-def debug (Object, Message):
-    Pieces = string.split (`Object`[1:-1], ' ');
-    Module, Name = string.split (Pieces [0], '.');
-    print time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.localtime()) + " " + Name + "@" + Pieces [len (Pieces) - 1] + ": " + Message; 
+def debug(Object, Message):
+    Pieces = string.split(`Object`[1:-1], ' ');
+    Module, Name = string.split(Pieces [0], '.');
+    print time.strftime("%a, %d %b %Y %H:%M:%S %Z", time.localtime()) + " " + Name + "@" + Pieces [len(Pieces) - 1] + ": " + Message; 
     
-def address (Addr):
+def address(Addr):
     return Addr [0] + ":" + `Addr [1]`;
 
-def resolve (Addr):
-    return (socket.gethostbyname (Addr[0]), Addr [1]);
+def resolve(Addr):
+    return(socket.gethostbyname(Addr[0]), Addr [1]);
 
 class protocol:
-    def __init__ (Self):
-        Self.Packets = {};
-        Self.Transformations = {"uint16":"H", "int16":"h", "str8":"8s", "str4": "4s", "str2": "2s"};
+    def __init__(self):
+        self.Packets = {};
+        self.Transformations = {"uint16":"H", "int16":"h", "str8":"8s", "str4": "4s", "str2": "2s"};
     
-    def load_protocol (Self, Packets):
-        Self.Packets = Packets;
+    def load_protocol(self, Packets):
+        self.Packets = Packets;
     
-    def make (Self, Message, Parameters):
+    def make(self, Message, Parameters):
         Buffer = Message + ":";
-        Bits = string.split (Self.Packets [Message], ' ');
+        Bits = string.split(self.Packets [Message], ' ');
         for Bit in Bits:
-            Name, Type = string.split (Bit, ':');
+            Name, Type = string.split(Bit, ':');
             if Type == '':
                 Buffer += Name
             else:
-                Buffer += struct.pack ("!" + Self.Transformations [Type], Parameters [Name])
+                Buffer += struct.pack("!" + self.Transformations [Type], Parameters [Name])
             
         return Buffer + "\x00";
     
-    def interpret (Self, Message):
-        Msg = Message [0: string.find (Message, ":")];
-        Data = Message [string.find (Message, ":") + 1: len (Message) - 1];
+    def interpret(self, Message):
+        Msg = Message [0: string.find(Message, ":")];
+        Data = Message [string.find(Message, ":") + 1: len(Message) - 1];
         UnpackString = "";
         Names = [];
-        Bits = string.split (Self.Packets [Msg], ' ');
+        Bits = string.split(self.Packets [Msg], ' ');
         for Bit in Bits:
-            Name, Type = string.split (Bit, ':');
-            UnpackString += Self.Transformations [Type];
-            Names.append (Name)
+            Name, Type = string.split(Bit, ':');
+            UnpackString += self.Transformations [Type];
+            Names.append(Name)
         Parameters = {};
-        Unpackage = struct.unpack ("!" + UnpackString, Data);
-        if len (Names) != len (Unpackage):
-            debug (Self, "oh my god something crazy has happened!");
+        Unpackage = struct.unpack("!" + UnpackString, Data);
+        if len(Names) != len(Unpackage):
+            debug(self, "oh my god something crazy has happened!");
         Ln = 0;
-        while (Ln < len (Unpackage)):
+        while(Ln < len(Unpackage)):
             Parameters [Names [Ln]] = Unpackage [Ln];
             Ln += 1
-        return (Msg, Parameters);
+        return(Msg, Parameters);
 
-class interaction (asyncore.dispatcher):
+class interaction(asyncore.dispatcher):
     STREAM = socket.SOCK_STREAM;
     PACKET = socket.SOCK_DGRAM;
 
-    def __init__ (Self, Typ, OnRead, OnWrite = 0, OnConnect = 0, OnDisconnect = 0):
-        Self.Host = ();
-        Self.Connected = 0;
+    def __init__(self, Typ, OnRead, OnWrite = 0, OnConnect = 0, OnDisconnect = 0):
+        self.Host =();
+        self.Connected = 0;
         if Typ is interaction.STREAM or Typ is interaction.PACKET:
-            asyncore.dispatcher.__init__ (Self);
-            Self.create_socket (socket.AF_INET, Typ);
+            asyncore.dispatcher.__init__(self);
+            self.create_socket(socket.AF_INET, Typ);
         else:
-            asyncore.dispatcher.__init__ (Self, Typ);
+            asyncore.dispatcher.__init__(self, Typ);
             try:
-                Self.Host = Typ.getpeername ();
-                Self.Connected = 1;
+                self.Host = Typ.getpeername();
+                self.Connected = 1;
             except:
                 pass;
-        Self.SocketBuffer = "";
-        Self.OnRead = OnRead;
-        Self.OnWrite = OnWrite;
-        Self.OnConnect = OnConnect;
-        Self.OnDisconnect = OnDisconnect;
-        Self.BufferSize = 256;
+        self.SocketBuffer = "";
+        self.OnRead = OnRead;
+        self.OnWrite = OnWrite;
+        self.OnConnect = OnConnect;
+        self.OnDisconnect = OnDisconnect;
+        self.BufferSize = 256;
 
     
-    def host (Self, Address = 0):
-        if (Self.Connected == 0) & (Address != 0):
-            Self.Host = Address;
-            Self.Host = (socket.gethostbyname (Self.Host [0]), Self.Host [1]);
-        return Self.Host;
+    def host(self, Address = 0):
+        if(self.Connected == 0) &(Address != 0):
+            self.Host = Address;
+            self.Host =(socket.gethostbyname(self.Host [0]), self.Host [1]);
+        return self.Host;
 
-    def listen (Self, Addr, Port, N = 1):
-        Self.bind ((Addr, Port));
-        asyncore.dispatcher.listen (Self, N);
+    def listen(self, Addr, Port, N = 1):
+        self.bind((Addr, Port));
+        asyncore.dispatcher.listen(self, N);
     
-    def write (Self, Buffer):
-        Self.SocketBuffer += Buffer;
+    def write(self, Buffer):
+        self.SocketBuffer += Buffer;
     
-    def writable (Self):
-        if (Self.OnWrite != 0) | len (Self.SocketBuffer):
+    def writable(self):
+        if(self.OnWrite != 0) | len(self.SocketBuffer):
             return 1;
         return 0;
             
-    def readable (Self):
-        if Self.OnRead or Self.OnConnect:
+    def readable(self):
+        if self.OnRead or self.OnConnect:
             return 1;
         return 0;
     
-    def connect_state (Self, OnConnect):
-        Self.OnConnect = OnConnect;
+    def connect_state(self, OnConnect):
+        self.OnConnect = OnConnect;
 
-    def disconnect_state (Self, OnDisconnect):
-        Self.OnDisconnect = OnDisconnect;
+    def disconnect_state(self, OnDisconnect):
+        self.OnDisconnect = OnDisconnect;
 
-    def write_state (Self, State):
-        Self.OnWrite = State;
+    def write_state(self, State):
+        self.OnWrite = State;
     
-    def read_state (Self, State):
-        Self.OnRead = State;
+    def read_state(self, State):
+        self.OnRead = State;
     
-    def connect (Self, To):
-        asyncore.dispatcher.connect (Self, To);
-        Self.Connected = 1;
-        Self.Host = To;
+    def connect(self, To):
+        asyncore.dispatcher.connect(self, To);
+        self.Connected = 1;
+        self.Host = To;
     
-    def handle_write (Self):
-        if len (Self.SocketBuffer) > 0:
-            if Self.Connected:
-                Sent = Self.send (Self.SocketBuffer);
+    def handle_write(self):
+        if len(self.SocketBuffer) > 0:
+            if self.Connected:
+                Sent = self.send(self.SocketBuffer);
             else:
-                Sent = Self.sendto (Self.SocketBuffer, Self.Host);
-            Self.SocketBuffer = Self.SocketBuffer [Sent:];
-        if (Self.OnWrite != 0):
-            New = Self.OnWrite ();
+                Sent = self.sendto(self.SocketBuffer, self.Host);
+            self.SocketBuffer = self.SocketBuffer [Sent:];
+        if(self.OnWrite != 0):
+            New = self.OnWrite();
             if New.__class__.__name__ != "NoneType":
-                Self.OnWrite = New;
+                self.OnWrite = New;
     
-    def handle_read (Self):
-        if (Self.Connected):
-            Data = Self.recv (Self.BufferSize);
-            Address = Self.Host;
+    def handle_read(self):
+        if(self.Connected):
+            Data = self.recv(self.BufferSize);
+            Address = self.Host;
         else:
-            Data, Address = Self.recvfrom (Self.BufferSize);
-        New = Self.OnRead (Data, Address);
+            Data, Address = self.recvfrom(self.BufferSize);
+        New = self.OnRead(Data, Address);
         if New.__class__.__name__ != "NoneType":
-            Self.OnRead = New;
+            self.OnRead = New;
 
-    def close (self):
-        if len (Self.SocketBuffer) > 0:
-            if Self.Connected:
-                Sent = Self.send (Self.SocketBuffer);
+    def close(self):
+        if len(self.SocketBuffer) > 0:
+            if self.Connected:
+                Sent = self.send(self.SocketBuffer);
             else:
-                Sent = Self.sendto (Self.SocketBuffer, Self.Host);
-        asyncore.dispatcher.close (self);
+                Sent = self.sendto(self.SocketBuffer, self.Host);
+        asyncore.dispatcher.close(self);
       
-    def handle_close (Self):
-        Self.close ();
-        if (Self.OnDisconnect):
-            Self.OnDisconnect ();
+    def handle_close(self):
+        self.close();
+        if(self.OnDisconnect):
+            self.OnDisconnect();
 
-    def handle_accept (Self):
-        if Self.OnConnect:
-            Con, Addr = Self.accept ();
-            Self.OnConnect (Con, Addr);
+    def handle_accept(self):
+        if self.OnConnect:
+            Con, Addr = self.accept();
+            self.OnConnect(Con, Addr);
 
-def run ():
-    asyncore.loop ();
+def run():
+    asyncore.loop();
--- robotfindskitten.py
+++ robotfindskitten.py
@@ -1,9 +1,39 @@
 #!/usr/bin/env python
 
-import metal, random, string, random;
+# Usage: robotfindskitten.py [port]
+# Starts a robotfindskitten on the given port (by default 2468).
 
-def mkc (attr, olda = []):
-    newattr = [];
+# To play game, connect using `telnet localhost {port}`
+
+# Copyright (c) 2003, 2011 by Samuel Williams.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import sys, metal, random, string;
+
+# {attr} 0 Reset All Attributes (return to normal mode) 1 Bright (Usually turns on BOLD)
+#  2 Dim 3 Underline 5 Blink 7 Reverse 8 Hidden
+# {fg} 30 Black 31 Red 32 Green 33 Yellow 34 Blue 35 Magenta 36 Cyan 37 White
+# {bg} 40	Black 41 Red 42 Green 43 Yellow 44 Blue 45 Magenta 46 Cyan 47 White
+
+def mkc(attr, olda =[]):
+    newattr =[];
     for e in attr:
         if e not in olda: newattr.append(e);
     if len(newattr):
@@ -13,185 +43,185 @@ def mkc (attr, olda = []):
         return buf[:-1] + "m", attr;
     return "", attr;
 
-class termcolor:
-    def __init__ (self):
+class TermColour:
+    def __init__(self):
         self.enable = True;
-        self.attributes = [0];
-        self.stack = [];
+        self.attributes =[0];
+        self.stack =[];
 
-    def enabled (self, yes = True):
+    def enabled(self, yes = True):
         self.enable = yes;
 
-    def push (self, attr = []):
+    def push(self, attr =[]):
         if self.enable:
-            bf, natr = mkc (attr, self.attributes);
-            self.stack.append (self.attributes);
+            bf, natr = mkc(attr, self.attributes);
+            self.stack.append(self.attributes);
             self.attributes = natr;
             return bf;
         return "";
 
-    def make (self, attr):
+    def make(self, attr):
         if self.enable:
-            bf, natr = mkc (attr, self.attributes);
+            bf, natr = mkc(attr, self.attributes);
             self.attributes = natr;
             return bf;
         return "";
 
-    def pop (self):
+    def pop(self):
         if self.enable:
-            bf, natr = mkc (self.stack.pop(), self.attributes);
+            bf, natr = mkc(self.stack.pop(), self.attributes);
             self.attributes = natr;
             return bf;
         return "";
 
-class robotserver (metal.interaction):
-    def __init__ (self, host = "localhost", port = 0):
+class RobotServer(metal.interaction):
+    def __init__(self, host = "localhost", port = 0):
         if port is 0:
-            port = random.randint (2468, 51015);
-        metal.interaction.__init__ (self, metal.interaction.STREAM, 0, 0, self.connect);
-        self.listen (host, port, 10);
-        metal.debug (self, "listening on " + host + ":" + `port`);
+            port = random.randint(2468, 51015);
+        metal.interaction.__init__(self, metal.interaction.STREAM, 0, 0, self.connect);
+        self.listen(host, port, 10);
+        metal.debug(self, "listening on " + host + ":" + `port`);
 
-    def connect (self, conn, address):
-        metal.debug (self, "got connect request from " + metal.address (address));
-        kittenclient (conn);
+    def connect(self, conn, address):
+        metal.debug(self, "got connect request from " + metal.address(address));
+        KittenClient(conn);
 
-def readflatrfkdata (file, append = "\r\n"):
+def read_flat_rfk_data(file, append = "\r\n"):
     bfr = "";
-    f = open (file);
+    f = open(file);
     for line in f.readlines():
         bfr += line[:-1] + append;
     return bfr;
 
-def readlistrfkdata (file, append = "\r\n"):
-    bfr = [];
-    f = open (file);
+def read_list_rfk_data(file, append = "\r\n"):
+    bfr =[];
+    f = open(file);
     for line in f.readlines():
-        bfr.append (line[:-1] + append);
+        bfr.append(line[:-1] + append);
     return bfr;
 
-hello_strings = readlistrfkdata ("greetings.rfk", "");
-goodbye_strings = readlistrfkdata ("goodbyes.rfk");
-story = readflatrfkdata ("story.rfk");
-help = readflatrfkdata ("help.rfk");
-findkitten = readflatrfkdata ("win.rfk");
-messages = readlistrfkdata ("messages.rfk");
+hello_strings = read_list_rfk_data("greetings.rfk", "");
+goodbye_strings = read_list_rfk_data("goodbyes.rfk");
+story = read_flat_rfk_data("story.rfk");
+help = read_flat_rfk_data("help.rfk");
+findkitten = read_flat_rfk_data("win.rfk");
+messages = read_list_rfk_data("messages.rfk");
 
-class kittenclient (metal.interaction):
-    def __init__ (self, socket):
-        metal.interaction.__init__ (self, socket, self.incoming, self.hello, 0, self.disconnect);
-        self.restart ();
-        self.color = termcolor ();
-        self.input = "";        
+class KittenClient(metal.interaction):
+    def __init__(self, socket):
+        metal.interaction.__init__(self, socket, self.incoming, self.hello, 0, self.disconnect);
+        self.restart();
+        self.color = TermColour();
+        self.input = ""
 
-    def restart (self):
-        size = random.randint (10, 25);
-        self.game = robotfindskitten (2 * size, size);
+    def restart(self):
+        size = random.randint(10, 25);
+        self.game = GameState(2 * size, size);
 
-    def win (self):
-        self.write (findkitten);
-        self.restart ();
+    def win(self):
+        self.write(findkitten);
+        self.restart();
 
-    def hello (self):
-        self.write ("version: kittenfindsrobot/0.2 [" + random.choice (hello_strings) + "]\r\n");
-        self.write ("words: help story quit color\r\n");
-        self.write ("press any key to start..\r\n");
+    def hello(self):
+        self.write("version: kittenfindsrobot/0.2[" + random.choice(hello_strings) + "]\r\n");
+        self.write("words: help story quit color\r\n");
+        self.write("press any key to start..\r\n");
+        self.write("\r\nUse characters E(up) C(down) S(left) F(right) for navigation.\r\n")
         return 0;
 
-    def incoming (self, data, address):
+    def incoming(self, data, address):
         self.input += data;
-        if len (self.input) == 0 or self.input[-1] != "\n":
+        if len(self.input) == 0 or self.input[-1] != "\n":
             return;
-        metal.debug (self, "command \n" );
         try:
-            command = string.split (string.strip (self.input), " ");
+            command = string.split(string.strip(self.input), " ");
             self.input = "";
             if command[0] == "story":
-                self.write (self.color.push ([35]) + "\r\n");
-                self.write (story);
-                self.write (self.color.pop () + "\r\n");
+                self.write(self.color.push([35]) + "\r\n");
+                self.write(story);
+                self.write(self.color.pop() + "\r\n");
                 return;
             if command[0] == "quit":
-                self.write (self.color.push ([35]) + "\r\n");
-                self.write (random.choice (goodbye_strings));
-                self.write (self.color.pop () + "\r\n");
-                self.close ();
+                self.write(self.color.push([35]) + "\r\n");
+                self.write(random.choice(goodbye_strings));
+                self.write(self.color.pop() + "\r\n");
+                self.close();
                 return;
             elif command[0] == "help":
-                self.write (self.color.push ([35]) + "\r\n");
-                self.write (help);
-                self.write (self.color.pop () + "\r\n");
+                self.write(self.color.push([35]) + "\r\n");
+                self.write(help);
+                self.write(self.color.pop() + "\r\n");
                 return;
             elif command[0] == "color":
                 if command[1] == "on":
-                    self.color.enabled (True);
-                    self.write ("color enabled\r\n");
+                    self.color.enabled(True);
+                    self.write("color enabled\r\n");
                 else:
-                    self.color.enabled (False);
-                    self.write ("color disabled\r\n");
+                    self.color.enabled(False);
+                    self.write("color disabled\r\n");
                 return;
             elif command[0] == "restart":
-                self.restart ();
+                self.restart();
                 return;
-            status = self.game.move (command[0]);
+            status = self.game.move(command[0]);
             if status == 0:
-                self.write (self.game.draw_field(self.color));
-                self.win ();
+                self.write(self.game.draw_field(self.color));
+                self.win();
                 return;
             elif status > 0:
-                self.write (messages[status]);
+                self.write(messages[status]);
             else:
-                self.write ("okay\r\n");
-            self.write (self.game.draw_field(self.color));
+                self.write("okay\r\n");
+            self.write(self.game.draw_field(self.color));
         except KeyboardInterrupt:
-            self.write ("bad input\r\n");
+            self.write("bad input\r\n");
             pass;
 
-    def disconnect (self):
-        metal.debug (self, "client " + metal.address (self.host()) + " disconnected");
+    def disconnect(self):
+        metal.debug(self, "client " + metal.address(self.host()) + " disconnected");
 
-class robotfindskitten:
-    def __init__ (self, wid, hei):
+class GameState:
+    def __init__(self, wid, hei):
         self.width, self.height = wid, hei;
-        self.rx, self.ry = int(wid/2), int (hei/2);
-        self.field = [];
-        for i in range (0, self.height):
-            row = [];
-            for j in range (0, self.width):
+        self.rx, self.ry = int(wid/2), int(hei/2);
+        self.field =[];
+        for i in range(0, self.height):
+            row =[];
+            for j in range(0, self.width):
                 #color, status, displaychar
-                #status = -1 (no message) = 0 (kitten here) = >0 item not kitten msg with number
-                row.append (([0, 40], -1, " "));
-            self.field.append (row);
+                #status = -1(no message) = 0(kitten here) = >0 item not kitten msg with number
+                row.append(([0], -1, " "));
+            self.field.append(row);
         placedkitten = False;
-        for p in range (0, int((self.width * self.height) / 32)):
-            x, y = random.randint (0, self.width-1), random.randint (0, self.height-1);
-            kittensymb = random.randint (1, len(messages) - 1);
+        for p in range(0, int((self.width * self.height) / 32)):
+            x, y = random.randint(0, self.width-1), random.randint(0, self.height-1);
+            kittensymb = random.randint(1, len(messages) - 1);
             if not placedkitten:
                 print "kitten=" + `x` + " " + `y`;
                 kittensymb = 0;
                 placedkitten = True;
-            charr = chr (random.randint (32, 127));
+            charr = chr(random.randint(32, 127));
             while charr == " ":
-                charr = chr (random.randint (32, 127));
-            self.field [y] [x] = ([random.randint (31,36), 40], kittensymb, charr);
+                charr = chr(random.randint(32, 127));
+            self.field[y][x] =([random.randint(31,36)], kittensymb, charr);
 
-    def draw_field (self, cl):
-        buffer = cl.push ([37, 40]);
-        buffer += "+" + ('-' * self.width) + "+\r\n";
-        for y in range (0, self.height):
-            buffer += cl.make ([37, 40]) + "|";
-            for x in range (0, self.width):
-                data = self.field [y] [x];
-                if (self.rx == x) and (self.ry == y):
-                    buffer += cl.make ([0, 37, 41]) + "#";
+    def draw_field(self, cl):
+        buffer = cl.push([37]);
+        buffer += "+" +('-' * self.width) + "+\r\n";
+        for y in range(0, self.height):
+            buffer += cl.make([37]) + "|";
+            for x in range(0, self.width):
+                data = self.field[y][x];
+                if(self.rx == x) and(self.ry == y):
+                    buffer += cl.make([0, 37]) + "#";
                 else:
-                    buffer += cl.make (data[0]) + data[2];
-            buffer += cl.make ([37, 40]) + "|\r\n";
-        buffer += cl.make ([37, 40]) + "+" + ('-' * self.width) + "+\r\n";
-        buffer += cl.pop () + "\r\n";
+                    buffer += cl.make(data[0]) + data[2];
+            buffer += cl.make([37]) + "|\r\n";
+        buffer += cl.make([37]) + "+" +('-' * self.width) + "+\r\n";
+        buffer += cl.pop() + "\r\n";
         return buffer;
 
-    def move (self, str):
+    def move(self, str):
         for c in str:
             if c == "e":
                 self.ry -= 1;
@@ -203,13 +233,18 @@ class robotfindskitten:
                 self.rx -= 1;
             self.rx = self.rx % self.width;
             self.ry = self.ry % self.height;
-            data = self.field [self.ry] [self.rx];
+            data = self.field[self.ry][self.rx];
             if data[1] != -1:
                 return data[1];
         return -1;
 
 try:
-    server = robotserver ("0.0.0.0", 2468);
-    metal.run ();
+    port = 2468
+    
+    if len(sys.argv) == 2:
+        port = int(sys.argv[1])
+    
+    server = RobotServer("0.0.0.0", port);
+    metal.run();
 except KeyboardInterrupt:
     server.close();
